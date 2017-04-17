0e309b12603fe8cdbdb54779d2008ec595944011
slyuan <cs.yuan777@hotmail.com>
2017-04-17 21:30:03 +0800

    add coding

--- src/main/spark/SparkTest.scala
+++ src/main/spark/SparkTest.scala
@@ -8,7 +8,7 @@ object SparkTest {
   def main(args: Array[String]) : Unit = {
     val conf = new SparkConf().setAppName("wordcount").setMaster("local")
     val sc = new SparkContext(conf)
-    val hdfsFile = sc.textFile("hdfs://localhost:9000//tmp/spark.txt")
+    val hdfsFile = sc.textFile("hdfs://localhost:9000/tmp/spark.txt")
     hdfsFile.flatMap(line => line.split("\\s+")).map(word => (word,1)).
       reduceByKey(_+_).saveAsTextFile("hdfs://localhost:9000/tmp/out")
   }
