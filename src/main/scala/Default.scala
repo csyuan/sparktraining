0ae0654730ebd88e1f388c51f8c6e44f1a043279
slyuan <cs.yuan777@hotmail.com>
2017-05-21 19:49:24 +0800

    add coding

--- src/main/scala/Default.scala
+++ src/main/scala/Default.scala
@@ -6,9 +6,9 @@ import scala.io.Source
   */
 object Default extends App{
 
-//    val flatMapRes = Source.fromFile("./data/textfile/spark.txt").getLines().toList.flatMap(line => line.split("\\s+"))
-//    val mapRes = Source.fromFile("./data/textfile/spark.txt").getLines().toList.flatMap(line => line.split("\\s+"))
-//    flatMapRes.foreach(println)
+    val flatMapRes = Source.fromFile("./data/textfile/spark.txt").getLines().toList.flatMap(line => line.split("\\s+"))
+    val mapRes = Source.fromFile("./data/textfile/spark.txt").getLines().toList.flatMap(line => line.split("\\s+"))
+    flatMapRes.foreach(println)
 
     //最后的toList是必须加的，因为getLines方法返回的是枚举器。
 }
